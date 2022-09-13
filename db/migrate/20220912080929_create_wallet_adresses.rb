ffaa085482cddb0f4fc04681cb07b795adfece03
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2022-09-13 16:29:10 +0900

    typoした名前のファイルを削除

--- db/migrate/20220912080929_create_wallet_adresses.rb
+++
@@ -1,9 +0,0 @@
-class CreateWalletAdresses < ActiveRecord::Migration[6.0]
-  def change
-    create_table :wallet_adresses do |t|
-      t.string :wallet_adress
-      t.references :user, foreign_key: true
-      t.timestamps
-    end
-  end
-end
